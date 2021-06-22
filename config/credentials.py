c9b10388fe40859088af82adcf1109208aa595c7
Richard Mwewa <r1chardmw3wa@gmail.com>
2021-06-22 11:59:57 +0200

    Create credentials.py

---
+++ config/credentials.py
@@ -0,0 +1,6 @@
+# Access keys can be obtained by applying for a twitter developer account here https://developer.twitter.com
+
+CONSUMER_KEY = "" # Consumer key(api key) 
+CONSUMER_SECRET = "" # Consumer secret 
+ACCESS_TOKEN = "" # access token 
+ACCESS_TOKEN_SECRET = "" # Access token secret 
