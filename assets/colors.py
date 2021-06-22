5e05de0b84fe75f8e6a5dd8f7a93e4723048787a
Richard Mwewa <r1chardmw3wa@gmail.com>
2021-06-22 02:38:29 +0200

    Add files via upload

---
+++ assets/colors.py
@@ -0,0 +1,9 @@
+# Defining colors
+white = "\033[97m"
+green = "\033[92m"
+red = "\033[91m"
+yellow = "\033[93m"
+end = "\033[0m"
+redBackground = "\033[0;37;41m"
+greenBackground = "\033[42;97m"
+brightred = "\033[31;1m"
