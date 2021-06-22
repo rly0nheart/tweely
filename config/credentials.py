e11d113a10f00049288cdf6dbd4cfeb90d63225a
Richard Mwewa <r1chardmw3wa@gmail.com>
2021-06-22 11:55:03 +0200

    Delete credentials.py

--- config/credentials.py
+++
@@ -1,6 +0,0 @@
-# The keys can be obtained by applying for a
-# twitter developer account here https://developer.twitter.com
-CONSUMER_KEY = "" #consumer key (api key) 
-CONSUMER_SECRET = "" # consumer secret
-ACCESS_TOKEN = "" # access token 
-ACCESS_TOKEN_SECRET = "" # access token secret
