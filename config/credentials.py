e66340c1939f334ef65943d70ff0ce31922e80c4
Richard Mwewa <r1chardmw3wa@gmail.com>
2021-06-22 02:40:22 +0200

    Add files via upload

---
+++ config/credentials.py
@@ -0,0 +1,6 @@
+# The keys can be obtained by applying for a
+# twitter developer account here https://developer.twitter.com
+CONSUMER_KEY = "2rrE0GVo0A00lLtSwEhFGCWFS"
+CONSUMER_SECRET = "i50ltyCOtfRbmwTyzrzlsKgfi3ni2acqVuZYC86IFGbGzHuoAz"
+ACCESS_TOKEN = "1056465455190339584-RqeZYqz2c9c0PPnTinwJahq2MoODRG"
+ACCESS_TOKEN_SECRET = "kE5UMMWcmhPZSm8GeBINywDtKKb6WzvTKmvNOQZvA6Qsd"
